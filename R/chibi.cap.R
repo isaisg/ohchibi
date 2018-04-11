62caa8e773423f272144ad3fa12f91a7c5438d24
isai <isai.salas.gonzalez@gmail.com>
2018-04-11 13:14:49 -0400

    Modify chibi.pco and chibi.cap to add shape plotting

--- R/chibi.cap.R
+++ R/chibi.cap.R
@@ -7,41 +7,61 @@
 #' chibi.cap()
 
 
-chibi.cap<-function(list_ohpco=NULL,col_val=NULL,comp_a=NULL,comp_b=NULL,mypch=22,size=25,alpha=0.7,stroke=1.5){
+chibi.cap<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a=NULL,comp_b=NULL,mypch=21,
+        size=25,alpha=0.7,stroke=1.5,col_shape_background="white",alpha_shape_background=0.7,
+        ratio_size_shape_background=1.3,y_vjust=0.5,x_hjust=0.5,size_axis_text=20,size_axis_title=30,
+        size_legend_text=20){
   percvar<-list_ohpco$variance_explained_axis
   Map_cap<-list_ohpco$Map_cap
   vartot<-list_ohpco$total_var
   pval<-list_ohpco$pval_model
-  p <- ggplot(data = Map_cap,aes_string(x = comp_a,y = comp_b))+
-    geom_point(size = size,alpha=alpha,pch=mypch,colour="#414141",stroke = stroke,aes(fill = get(col_val)))+
-    #scale_fill_manual(values = mix.colors)+
-    xlab(label = paste(comp_a,"(",percvar[which(names(percvar)==comp_a)],"%)",sep="")) + 
-    ylab(label = paste(comp_b,"(",percvar[which(names(percvar)==comp_b)],"%)",sep="")) +
-    guides(fill = guide_legend(override.aes = list(size=12))) +
-    theme(axis.line = element_blank(),
-          panel.background = element_rect(fill = 'white'),
-          panel.grid.major = element_line(colour =   "#D9D9D9"),
-          panel.grid.minor = element_line(colour = "#D9D9D9"),
-          panel.border = element_rect(fill=NA,color =  "#414141",size = 1),
-          axis.ticks = element_line(colour = "black",size = 2.5),
-          axis.text.x = element_text(family = "AvantGarde",face = "plain",size =20,colour="#414141"),
-          axis.text.y = element_text(family = "AvantGarde",face="plain",size=20,colour="#414141"),
-          axis.title.x = element_text(family = "AvantGarde",face="plain",size = 30,colour = "#414141"),
-          axis.title.y = element_text(family = "AvantGarde",face="plain",size=30,colour="#414141"),
-          legend.background = element_blank(),legend.key.size = unit(2,"point"),
-          legend.title=element_blank(),legend.key = element_blank(), 
-          legend.text = element_text(size=20,
-                                     family = "AvantGarde",face = "plain",colour = "#414141"),
-          legend.position ="right") 
-  p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
-  #Add the total variance explained and the significance as a title for the plot
-  #if(pval <0.001){
-  #  p<-p + theme(plot.title = element_text(family = 'AvantGarde',colour="#414141",hjust=1,size = 30)) +labs(title=paste(format(vartot, digits=3), " % of total variance, p-value<0.001",sep=""))
-  #  
-  #}else{
-  #  p<-p + theme(plot.title = element_text(family = 'AvantGarde',colour="#414141",hjust=1,size = 30)) +labs(title=paste(format(vartot, digits=3), " % of total variance, p-value=",format(pval, digits=3),sep=""))
-  #  
-  #}
-  return(p)
+    if(is.null(shape_val)){
+    p <- ggplot(data = Map_cap,aes_string(x = comp_a,y = comp_b))+
+      geom_point(size = size,alpha=alpha,pch=mypch,colour="#414141",stroke = stroke,aes_string(fill = col_val))+
+      xlab(label = paste(comp_a,"(",percvar[which(names(percvar)==comp_a)],"%)",sep="")) + 
+      ylab(label = paste(comp_b,"(",percvar[which(names(percvar)==comp_b)],"%)",sep="")) +
+      guides(fill = guide_legend(override.aes = list(size=12))) +
+      theme(axis.line = element_blank(),
+            panel.background = element_rect(fill = 'white'),
+            panel.grid.major = element_line(colour =   "#D9D9D9"),
+            panel.grid.minor = element_line(colour = "#D9D9D9"),
+            panel.border = element_rect(fill=NA,color =  "#414141",size = 1),
+            axis.ticks = element_line(colour = "black",size = 2.5),
+            axis.text.x = element_text(family = "AvantGarde",face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
+            axis.text.y = element_text(family = "AvantGarde",face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
+            axis.title.x = element_text(family = "AvantGarde",face="plain",size = size_axis_title,colour = "#414141"),
+            axis.title.y = element_text(family = "AvantGarde",face="plain",size=size_axis_title,colour="#414141"),
+            legend.background = element_blank(),legend.key.size = unit(2,"point"),
+            legend.title=element_blank(),legend.key = element_blank(), 
+            legend.text = element_text(size=size_legend_text,
+                                       family = "AvantGarde",face = "plain",colour = "#414141"),
+            legend.position ="right") 
+    p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
+    return(p)
+    }else{
+      p <- ggplot(data = Map_cap,aes_string(x = comp_a,y = comp_b,shape = shape_val))+
+        geom_point(size = size,alpha=alpha,aes_string(colour = col_val))+
+        geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+ 
+        xlab(label = paste(comp_a,"(",percvar[which(names(percvar)==comp_a)],"%)",sep="")) + 
+        ylab(label = paste(comp_b,"(",percvar[which(names(percvar)==comp_b)],"%)",sep="")) +
+        guides(fill = guide_legend(override.aes = list(size=12))) +
+        theme(axis.line = element_blank(),
+              panel.background = element_rect(fill = 'white'),
+              panel.grid.major = element_line(colour =   "#D9D9D9"),
+              panel.grid.minor = element_line(colour = "#D9D9D9"),
+              panel.border = element_rect(fill=NA,color =  "#414141",size = 1),
+              axis.ticks = element_line(colour = "black",size = 2.5),
+              axis.text.x = element_text(family = "AvantGarde",face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
+              axis.text.y = element_text(family = "AvantGarde",face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
+              axis.title.x = element_text(family = "AvantGarde",face="plain",size = size_axis_title,colour = "#414141"),
+              axis.title.y = element_text(family = "AvantGarde",face="plain",size=size_axis_title,colour="#414141"),
+              legend.background = element_blank(),legend.key.size = unit(2,"point"),
+              legend.title=element_blank(),legend.key = element_blank(), 
+              legend.text = element_text(size=size_legend_text,
+                                         family = "AvantGarde",face = "plain",colour = "#414141"),
+              legend.position ="right") 
+      p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
+      return(p)
+    }
 }
 
--- R/chibi.pco.R
+++ R/chibi.pco.R
@@ -7,7 +7,10 @@
 #' chibi.pco()
 
 
-chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",comp_b="PCo2",mypch=22,size=25,alpha=0.7,stroke=1.5){
+chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",comp_b="PCo2",mypch=21,
+                    size=25,alpha=0.7,stroke=1.5,col_shape_background="white",alpha_shape_background=0.7,
+                    ratio_size_shape_background=1.3,y_vjust=0.5,x_hjust=0.5,size_axis_text=20,size_axis_title=30,
+                    size_legend_text=20){
   Map_pco<-list_ohpco$Map_pco
   myvar=list_ohpco$variance_explained
   if(is.null(shape_val)){
@@ -23,20 +26,21 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
             panel.grid.minor = element_line(colour = "#D9D9D9"),
             panel.border = element_rect(fill=NA,color =  "#414141",size = 1),
             axis.ticks = element_line(colour = "black",size = 2.5),
-            axis.text.x = element_text(family = "AvantGarde",face = "plain",size =20,colour="#414141"),
-            axis.text.y = element_text(family = "AvantGarde",face="plain",size=20,colour="#414141"),
-            axis.title.x = element_text(family = "AvantGarde",face="plain",size = 30,colour = "#414141"),
-            axis.title.y = element_text(family = "AvantGarde",face="plain",size=30,colour="#414141"),
+            axis.text.x = element_text(family = "AvantGarde",face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
+            axis.text.y = element_text(family = "AvantGarde",face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
+            axis.title.x = element_text(family = "AvantGarde",face="plain",size = size_axis_title,colour = "#414141"),
+            axis.title.y = element_text(family = "AvantGarde",face="plain",size=size_axis_title,colour="#414141"),
             legend.background = element_blank(),legend.key.size = unit(2,"point"),
             legend.title=element_blank(),legend.key = element_blank(), 
-            legend.text = element_text(size=20,
+            legend.text = element_text(size=size_legend_text,
                                        family = "AvantGarde",face = "plain",colour = "#414141"),
             legend.position ="right") 
     p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
     return(p)
   }else{
-    p <- ggplot(data = Map_pco,aes_string(x = comp_a,y = comp_b))+
-      geom_point(size = size,alpha=alpha,stroke = stroke,aes_string(colour = col_val,shape = shape_val))+
+    p <- ggplot(data = Map_pco,aes_string(x = comp_a,y = comp_b,shape=shape_val))+
+      geom_point(size = size,alpha=alpha,aes_string(colour = col_val))+ 
+      geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+ 
       xlab(label = paste(comp_a,"(",myvar[which(names(myvar)==comp_a)],"%)",sep="")) + 
       ylab(label = paste(comp_b,"(",myvar[which(names(myvar)==comp_b)],"%)",sep="")) +
       guides(fill = guide_legend(override.aes = list(size=12))) +
@@ -46,13 +50,13 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
             panel.grid.minor = element_line(colour = "#D9D9D9"),
             panel.border = element_rect(fill=NA,color =  "#414141",size = 1),
             axis.ticks = element_line(colour = "black",size = 2.5),
-            axis.text.x = element_text(family = "AvantGarde",face = "plain",size =20,colour="#414141"),
-            axis.text.y = element_text(family = "AvantGarde",face="plain",size=20,colour="#414141"),
-            axis.title.x = element_text(family = "AvantGarde",face="plain",size = 30,colour = "#414141"),
-            axis.title.y = element_text(family = "AvantGarde",face="plain",size=30,colour="#414141"),
+            axis.text.x = element_text(family = "AvantGarde",face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
+            axis.text.y = element_text(family = "AvantGarde",face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
+            axis.title.x = element_text(family = "AvantGarde",face="plain",size = size_axis_title,colour = "#414141"),
+            axis.title.y = element_text(family = "AvantGarde",face="plain",size=size_axis_title,colour="#414141"),
             legend.background = element_blank(),legend.key.size = unit(2,"point"),
             legend.title=element_blank(),legend.key = element_blank(), 
-            legend.text = element_text(size=20,
+            legend.text = element_text(size=size_legend_text,
                                        family = "AvantGarde",face = "plain",colour = "#414141"),
             legend.position ="right") 
     p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
