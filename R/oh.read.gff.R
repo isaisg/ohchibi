1fc2aca4efcb4f5859099ab808120c3a698913a2
isai <isai.salas.gonzalez@gmail.com>
2018-09-26 22:41:24 -0400

    Allow gff to have orthogroups

--- R/oh.read.gff.R
+++ R/oh.read.gff.R
@@ -9,27 +9,53 @@
 
 
 
-oh.read.gff <- function(file = NULL){
-  tfile <- read.table(file = file,header = F,
-             skip = 1,comment.char = "",quote = "",sep = "\t")
+oh.read.gff <- function(file = NULL,withOGs = FLASE){
+  if(withOGs == TRUE){
+    tfile <- read.table(file = file,header = F,
+          skip = 1,comment.char = "",quote = "",sep = "\t")
   colnames(tfile) <-  c("seqid", "source", "type", "start", "end", 
-                        "score", "strand", "phase", "attributes")
-  gid <- NULL
-  glocus <- NULL
-  for(i in 1:nrow(tfile)){
-    #Check if the type is crispr
-    if(tfile$type[i] == "CRISPR"){
-      gid <- c(gid,NA)
-      glocus <- c(glocus,NA)
-    }else{
-      gid <- c(gid,gsub(pattern = "ID=(.*?)\\;locus_tag=(.*?)\\;.*",
-                        replacement ="\\1",tfile$attributes[i]))
-      glocus <- c(glocus,gsub(pattern = "ID=(.*?)\\;locus_tag=(.*?)\\;.*",
-                              replacement ="\\2",tfile$attributes[i]))
+                        "score", "strand", "phase", "attributes","orthogroup_id")
+    gid <- NULL
+    glocus <- NULL
+    for(i in 1:nrow(tfile)){
+      #Check if the type is crispr
+      if(tfile$type[i] == "CRISPR"){
+        gid <- c(gid,NA)
+        glocus <- c(glocus,NA)
+      }else{
+        gid <- c(gid,gsub(pattern = "ID=(.*?)\\;locus_tag=(.*?)\\;.*",
+                          replacement ="\\1",tfile$attributes[i]))
+        glocus <- c(glocus,gsub(pattern = "ID=(.*?)\\;locus_tag=(.*?)\\;.*",
+                                replacement ="\\2",tfile$attributes[i]))
+      }
     }
+    tfile$gene_oid <- factor(gid)
+    tfile$locus_tag <- factor(glocus)
+    return(tfile)
+
+  }else{
+    tfile <- read.table(file = file,header = F,
+               skip = 1,comment.char = "",quote = "",sep = "\t")
+    colnames(tfile) <-  c("seqid", "source", "type", "start", "end", 
+                          "score", "strand", "phase", "attributes")
+    gid <- NULL
+    glocus <- NULL
+    for(i in 1:nrow(tfile)){
+      #Check if the type is crispr
+      if(tfile$type[i] == "CRISPR"){
+        gid <- c(gid,NA)
+        glocus <- c(glocus,NA)
+      }else{
+        gid <- c(gid,gsub(pattern = "ID=(.*?)\\;locus_tag=(.*?)\\;.*",
+                          replacement ="\\1",tfile$attributes[i]))
+        glocus <- c(glocus,gsub(pattern = "ID=(.*?)\\;locus_tag=(.*?)\\;.*",
+                                replacement ="\\2",tfile$attributes[i]))
+      }
+    }
+    tfile$gene_oid <- factor(gid)
+    tfile$locus_tag <- factor(glocus)
+    return(tfile)
   }
-  tfile$gene_oid <- factor(gid)
-  tfile$locus_tag <- factor(glocus)
-  return(tfile)
+
 }
 
