7043db0c6787e6888d85907441fa07ab2899dcd6
isaisg <isai.salas.gonzalez@gmail.com>
2018-11-17 19:31:31 -0500

    Modify oh.save.pdf

--- R/oh.save.pdf.R
+++ R/oh.save.pdf.R
@@ -6,16 +6,14 @@
 #' @examples
 #' oh.save.pdf()
 
-oh.save.pdf <- function(object = NULL,outname = NULL,outdir = "figures/",
-                        width = 20, height = 15, family = "Arial",fallback_resolution = 1200,
+oh.save.pdf <- function(p = NULL,outname = NULL,outdir = "figures/",
+                        width = 20, height = 15, family = "Arial", fallback_resolution = 1200,
                         antialias = "default"){
   dir.create(outdir, showWarnings = FALSE)
   myfilename <- paste(outdir, outname, sep = "/")
-  cairo_pdf(filename  = myfilename,
-            onefile = FALSE,fallback_resolution = fallback_resolution,
-            width = width,height = height,family = family,
-            antialias = antialias)
-  object
+  cairo_pdf(filename = myfilename, onefile = FALSE, fallback_resolution = fallback_resolution, 
+          width = width, height = height, family = family, antialias = antialias)
+  p
   dev.off()
 
 }
