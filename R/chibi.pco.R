bfaad4b794b25c545f637de4e4c3e8bc127f2bc4
isaisg <isai.salas.gonzalez@gmail.com>
2018-11-17 22:38:52 -0500

    Add linez_zero to chibi pco

--- R/chibi.pco.R
+++ R/chibi.pco.R
@@ -9,7 +9,7 @@
 
 chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",comp_b="PCo2",mypch=21,
                     size=25,alpha=0.7,stroke=1.5,col_shape_background="white",alpha_shape_background=0,
-                    ines_zero = FALSE,size_axis_line=0.5,type_axis_line = "longdash",
+                    lines_zero = FALSE,size_axis_line=0.5,type_axis_line = "longdash",
                     ratio_size_shape_background=1.3,y_vjust=0.5,x_hjust=0.5,size_axis_text=20,size_axis_title=30,
                     size_legend_text=20,size_title_text = 30,legend_proportion_size=2,
 		size_lines_panel = 0,size_panel_border = 1,font_family = "Arial"){
