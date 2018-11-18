bedea6a45bb64c000b3a33c393b76348b3859517
isaisg <isai.salas.gonzalez@gmail.com>
2018-11-17 21:56:19 -0500

    Add type of line for the axis in chibi pco and chibi cap. As well remove the lines drawn acroos all axis.

--- R/chibi.pco.R
+++ R/chibi.pco.R
@@ -8,10 +8,11 @@
 
 
 chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",comp_b="PCo2",mypch=21,
-                    size=25,alpha=0.7,stroke=1.5,col_shape_background="white",alpha_shape_background=0,lines_zero = FALSE,size_axis_line=0.5,
+                    size=25,alpha=0.7,stroke=1.5,col_shape_background="white",alpha_shape_background=0,
+                    ines_zero = FALSE,size_axis_line=0.5,type_axis_line = "longdash",
                     ratio_size_shape_background=1.3,y_vjust=0.5,x_hjust=0.5,size_axis_text=20,size_axis_title=30,
                     size_legend_text=20,size_title_text = 30,legend_proportion_size=2,
-		size_lines_panel = 0.3,size_panel_border = 1,font_family = "Arial"){
+		size_lines_panel = 0,size_panel_border = 1,font_family = "Arial"){
   Map_pco<-list_ohpco$Map_pco
   myvar=list_ohpco$variance_explained
   if(lines_zero == FALSE) {
@@ -70,8 +71,8 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
   }else{
   if(is.null(shape_val)){
     p <- ggplot(data = Map_pco,aes_string(x = comp_a,y = comp_b))+
-    geom_vline(xintercept = 0,size = size_axis_line,color = "#D9D9D9") +
-    geom_hline(yintercept = 0,size = size_axis_line,color = "#D9D9D9") +
+    geom_vline(xintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
+    geom_hline(yintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
       geom_point(size = size,alpha=alpha,pch=mypch,colour="#414141",stroke = stroke,aes(fill = get(col_val)))+
       geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+
       #scale_fill_manual(values = mix.colors)+
@@ -98,8 +99,8 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
     return(p)
   }else{
     p <- ggplot(data = Map_pco,aes_string(x = comp_a,y = comp_b,shape=shape_val))+
-    geom_vline(xintercept = 0,size = size_axis_line,color = "#D9D9D9") +
-    geom_hline(yintercept = 0,size = size_axis_line,color = "#D9D9D9") +
+    geom_vline(xintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
+    geom_hline(yintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
       geom_point(size = size,alpha=alpha,aes_string(colour = col_val))+
       geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+
       xlab(label = paste(comp_a,"(",myvar[which(names(myvar)==comp_a)],"%)",sep="")) +
