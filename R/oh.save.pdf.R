ef26ddf8bc5628a11a85b8341aba2153d178b672
isaisg <isai.salas.gonzalez@gmail.com>
2018-11-19 10:53:26 -0500

    Add poitnsize option to oh.save.pdf

--- R/oh.save.pdf.R
+++ R/oh.save.pdf.R
@@ -1,6 +1,6 @@
 #' Function to save objects in high resolution. To be used with illustrator
 #'
-#' Pass any type of figure. 
+#' Pass any type of figure.
 #' @keywords save,figure,illustrator,vectorized
 #' @export
 #' @examples
@@ -8,11 +8,11 @@
 
 oh.save.pdf <- function(p = NULL,outname = NULL,outdir = "figures/",
                         width = 20, height = 15, family = "Arial", fallback_resolution = 1200,
-                        antialias = "default"){
+                        antialias = "default",pointsize = 12){
   dir.create(outdir, showWarnings = FALSE)
   myfilename <- paste(outdir, outname, sep = "/")
-  cairo_pdf(filename = myfilename, onefile = FALSE, fallback_resolution = fallback_resolution, 
-          width = width, height = height, family = family, antialias = antialias)
+  cairo_pdf(filename = myfilename, onefile = FALSE, fallback_resolution = fallback_resolution,
+          width = width, height = height, family = family, antialias = antialias,pointsize = pointsize)
   print(p)
   dev.off()
 
