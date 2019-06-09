28a91fec6bedbbab8b9f816ba85e2ae463b6d07f
isaisg <isai.salas.gonzalez@gmail.com>
2019-06-09 14:41:01 -0400

    Fix theme

--- R/theme_ohchibi.R
+++ R/theme_ohchibi.R
@@ -12,8 +12,8 @@ theme_ohchibi <- function (size_axis_text.x = 20,size_axis_text.y = 20,
                            angle_text.x = 0,angle_text.y = 0,
                            legend_proportion_size = 1,
                            size_title_text = 30,size_legend_text = 20,
-                           size_lines_panel = 0.3,size_panel_border = 2,
-                           x_hjust = 0.5, y_vjust = 0.5,font_family = "Arial",size_ticks = 2) { 
+                           size_lines_panel = 0.3,size_panel_border = 1,
+                           x_hjust = 0.5, y_vjust = 0.5,font_family = "Arial",size_ticks = 1.5) { 
   theme(
         panel.background = element_rect(fill = 'white'),
         panel.grid.major.y = element_blank(),
