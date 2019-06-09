192d199be949e99a63e800ca3a4c15c4785609b0
isaisg <isai.salas.gonzalez@gmail.com>
2019-06-09 14:26:34 -0400

    Modify theme

--- R/theme_ohchibi.R
+++ R/theme_ohchibi.R
@@ -15,14 +15,14 @@ theme_ohchibi <- function (size_axis_text.x = 20,size_axis_text.y = 20,
                            size_strip_text.x = 30,
                            size_strip_text.y = 30,
                            size_lines_panel = 0.3,size_panel_border = 2,
-                           x_hjust = 0.5, y_vjust = 0.5,font_family = "Arial") { 
+                           x_hjust = 0.5, y_vjust = 0.5,font_family = "Arial",size_ticks = 2) { 
   theme(axis.line = element_blank(),
         panel.background = element_rect(fill = 'white'),
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
 	panel.grid.major.x = element_blank(),
         panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
-        axis.ticks = element_line(colour = "black",size = 2.5),
+        axis.ticks = element_line(colour = "black",size = size_ticks),
         axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text.x,colour="black",hjust = x_hjust,angle = angle_text.x),
         axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text.y,colour="black",vjust = y_vjust, angle = angle_text.y),
         axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title.x,colour = "black"),
@@ -34,8 +34,8 @@ theme_ohchibi <- function (size_axis_text.x = 20,size_axis_text.y = 20,
         legend.text = element_text(size=size_legend_text,
           family = font_family,face = "bold",colour = "black"),
         legend.position ="right",
-        strip.text.x = element_text(family = font_family,colour = "black",face = "bold", size = size_strip_text.x),
-        strip.text.y = element_text(family = font_family,colour = "black",face = "bold", size = size_strip_text.y),
+        #strip.text.x = element_text(family = font_family,colour = "black",face = "bold", size = size_strip_text.x),
+        #strip.text.y = element_text(family = font_family,colour = "black",face = "bold", size = size_strip_text.y),
           strip.background = element_blank()
         )
 }
