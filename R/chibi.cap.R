9bcf3f3575a6bb26f232787932efa74ffb58b780
isaisg <isai.salas.gonzalez@gmail.com>
2019-07-15 11:09:46 -0400

    Change colors and bold lines in chibi.cap , chibi.pco, chibi.boxplot and chibi.phylogram

--- R/chibi.cap.R
+++ R/chibi.cap.R
@@ -12,14 +12,14 @@ chibi.cap<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a=NULL,comp
         lines_zero = FALSE,size_axis_line = 0.5,type_axis_line = "longdash",
         ratio_size_shape_background=1.3,y_vjust=0.5,x_hjust=0.5,size_axis_text=20,size_axis_title=30,
         size_legend_text=20,size_title_text = 30,legend_proportion_size=2,size_lines_panel = 0,size_panel_border = 1,font_family = "Arial"){
-  percvar<-list_ohpco$variance_explained_axis
+  percvar<-list_ohpco$variance_exbolded_axis
   Map_cap<-list_ohpco$Map_cap
   vartot<-list_ohpco$total_var
   pval<-list_ohpco$pval_model
   if(lines_zero == FALSE) {
     if(is.null(shape_val)){
     p <- ggplot(data = Map_cap,aes_string(x = comp_a,y = comp_b))+
-      geom_point(size = size,alpha=alpha,pch=mypch,colour="#414141",stroke = stroke,aes_string(fill = col_val))+
+      geom_point(size = size,alpha=alpha,pch=mypch,colour="black",stroke = stroke,aes_string(fill = col_val))+
       geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+
       xlab(label = paste(comp_a,"(",percvar[which(names(percvar)==comp_a)],"%)",sep="")) +
       ylab(label = paste(comp_b,"(",percvar[which(names(percvar)==comp_b)],"%)",sep="")) +
@@ -28,18 +28,18 @@ chibi.cap<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a=NULL,comp
             panel.background = element_rect(fill = 'white'),
             panel.grid.major = element_line(colour =   "#D9D9D9",size = size_lines_panel),
             panel.grid.minor = element_line(colour = "#D9D9D9", size = size_lines_panel),
-            panel.border = element_rect(fill=NA,color =  "#414141",size = size_panel_border),
+            panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
             axis.ticks = element_line(colour = "black",size = 2.5),
-            axis.text.x = element_text(family = font_family,face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
-            axis.text.y = element_text(family = font_family,face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
-            axis.title.x = element_text(family = font_family,face="plain",size = size_axis_title,colour = "#414141"),
-            axis.title.y = element_text(family = font_family,face="plain",size=size_axis_title,colour="#414141"),
+            axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text,colour="black",hjust = x_hjust),
+            axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text,colour="black",vjust = y_vjust),
+            axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title,colour = "black"),
+            axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title,colour="black"),
             legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
             legend.title=element_text(size=size_title_text,
-          family = font_family,face = "plain",colour = "#414141"),
+          family = font_family,face = "bold",colour = "black"),
             legend.key = element_blank(),
             legend.text = element_text(size=size_legend_text,
-                                       family = font_family,face = "plain",colour = "#414141"),
+                                       family = font_family,face = "bold",colour = "black"),
             legend.position ="right")
     #p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
     return(p)
@@ -54,18 +54,18 @@ chibi.cap<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a=NULL,comp
               panel.background = element_rect(fill = 'white'),
               panel.grid.major = element_line(colour =   "#D9D9D9",size = size_lines_panel),
               panel.grid.minor = element_line(colour = "#D9D9D9", size = size_lines_panel),
-              panel.border = element_rect(fill=NA,color =  "#414141",size = size_panel_border),
+              panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
               axis.ticks = element_line(colour = "black",size = 2.5),
-              axis.text.x = element_text(family = font_family,face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
-              axis.text.y = element_text(family = font_family,face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
-              axis.title.x = element_text(family = font_family,face="plain",size = size_axis_title,colour = "#414141"),
-              axis.title.y = element_text(family = font_family,face="plain",size=size_axis_title,colour="#414141"),
+              axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text,colour="black",hjust = x_hjust),
+              axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text,colour="black",vjust = y_vjust),
+              axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title,colour = "black"),
+              axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title,colour="black"),
               legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
               legend.title=element_text(size=size_title_text,
-          family = font_family,face = "plain",colour = "#414141"),
+          family = font_family,face = "bold",colour = "black"),
               legend.key = element_blank(),
               legend.text = element_text(size=size_legend_text,
-                                         family = font_family,face = "plain",colour = "#414141"),
+                                         family = font_family,face = "bold",colour = "black"),
               legend.position ="right")
       #p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
       return(p)
@@ -75,7 +75,7 @@ chibi.cap<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a=NULL,comp
   p <- ggplot(data = Map_cap,aes_string(x = comp_a,y = comp_b))+
     geom_vline(xintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
     geom_hline(yintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
-    geom_point(size = size,alpha=alpha,pch=mypch,colour="#414141",stroke = stroke,aes_string(fill = col_val))+
+    geom_point(size = size,alpha=alpha,pch=mypch,colour="black",stroke = stroke,aes_string(fill = col_val))+
     geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+
     xlab(label = paste(comp_a,"(",percvar[which(names(percvar)==comp_a)],"%)",sep="")) +
     ylab(label = paste(comp_b,"(",percvar[which(names(percvar)==comp_b)],"%)",sep="")) +
@@ -84,18 +84,18 @@ chibi.cap<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a=NULL,comp
           panel.background = element_rect(fill = 'white'),
           panel.grid.major = element_line(colour =   "#D9D9D9",size = size_lines_panel),
           panel.grid.minor = element_line(colour = "#D9D9D9", size = size_lines_panel),
-          panel.border = element_rect(fill=NA,color =  "#414141",size = size_panel_border),
+          panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
           axis.ticks = element_line(colour = "black",size = 2.5),
-          axis.text.x = element_text(family = font_family,face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
-          axis.text.y = element_text(family = font_family,face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
-          axis.title.x = element_text(family = font_family,face="plain",size = size_axis_title,colour = "#414141"),
-          axis.title.y = element_text(family = font_family,face="plain",size=size_axis_title,colour="#414141"),
+          axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text,colour="black",hjust = x_hjust),
+          axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text,colour="black",vjust = y_vjust),
+          axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title,colour = "black"),
+          axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title,colour="black"),
           legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
           legend.title=element_text(size=size_title_text,
-        family = font_family,face = "plain",colour = "#414141"),
+        family = font_family,face = "bold",colour = "black"),
           legend.key = element_blank(),
           legend.text = element_text(size=size_legend_text,
-                                     family = font_family,face = "plain",colour = "#414141"),
+                                     family = font_family,face = "bold",colour = "black"),
           legend.position ="right")
   #p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
   return(p)
@@ -112,18 +112,18 @@ chibi.cap<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a=NULL,comp
             panel.background = element_rect(fill = 'white'),
             panel.grid.major = element_line(colour =   "#D9D9D9",size = size_lines_panel),
             panel.grid.minor = element_line(colour = "#D9D9D9", size = size_lines_panel),
-            panel.border = element_rect(fill=NA,color =  "#414141",size = size_panel_border),
+            panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
             axis.ticks = element_line(colour = "black",size = 2.5),
-            axis.text.x = element_text(family = font_family,face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
-            axis.text.y = element_text(family = font_family,face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
-            axis.title.x = element_text(family = font_family,face="plain",size = size_axis_title,colour = "#414141"),
-            axis.title.y = element_text(family = font_family,face="plain",size=size_axis_title,colour="#414141"),
+            axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text,colour="black",hjust = x_hjust),
+            axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text,colour="black",vjust = y_vjust),
+            axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title,colour = "black"),
+            axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title,colour="black"),
             legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
             legend.title=element_text(size=size_title_text,
-        family = font_family,face = "plain",colour = "#414141"),
+        family = font_family,face = "bold",colour = "black"),
             legend.key = element_blank(),
             legend.text = element_text(size=size_legend_text,
-                                       family = font_family,face = "plain",colour = "#414141"),
+                                       family = font_family,face = "bold",colour = "black"),
             legend.position ="right")
     #p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
     return(p)
--- R/chibi.pco.R
+++ R/chibi.pco.R
@@ -14,11 +14,11 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
                     size_legend_text=20,size_title_text = 30,legend_proportion_size=2,
 		size_lines_panel = 0,size_panel_border = 1,font_family = "Arial"){
   Map_pco<-list_ohpco$Map_pco
-  myvar=list_ohpco$variance_explained
+  myvar=list_ohpco$variance_exbolded
   if(lines_zero == FALSE) {
     if(is.null(shape_val)){
       p <- ggplot(data = Map_pco,aes_string(x = comp_a,y = comp_b))+
-        geom_point(size = size,alpha=alpha,pch=mypch,colour="#414141",stroke = stroke,aes(fill = get(col_val)))+
+        geom_point(size = size,alpha=alpha,pch=mypch,colour="black",stroke = stroke,aes(fill = get(col_val)))+
         geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+
         #scale_fill_manual(values = mix.colors)+
         xlab(label = paste(comp_a,"(",myvar[which(names(myvar)==comp_a)],"%)",sep="")) +
@@ -28,17 +28,17 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
               panel.background = element_rect(fill = 'white'),
               panel.grid.major = element_line(colour =   "#D9D9D9",size = size_lines_panel),
               panel.grid.minor = element_line(colour = "#D9D9D9", size = size_lines_panel),
-              panel.border = element_rect(fill=NA,color =  "#414141",size = size_panel_border),
+              panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
               axis.ticks = element_line(colour = "black",size = 2.5),
-              axis.text.x = element_text(family = font_family,face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
-              axis.text.y = element_text(family = font_family,face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
-              axis.title.x = element_text(family = font_family,face="plain",size = size_axis_title,colour = "#414141"),
-              axis.title.y = element_text(family = font_family,face="plain",size=size_axis_title,colour="#414141"),
+              axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text,colour="black",hjust = x_hjust),
+              axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text,colour="black",vjust = y_vjust),
+              axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title,colour = "black"),
+              axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title,colour="black"),
               legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
               legend.title=element_text(size=size_title_text,
-            family = font_family,face = "plain",colour = "#414141"),            legend.key = element_blank(),
+            family = font_family,face = "bold",colour = "black"),            legend.key = element_blank(),
               legend.text = element_text(size=size_legend_text,
-                                         family = font_family,face = "plain",colour = "#414141"),
+                                         family = font_family,face = "bold",colour = "black"),
               legend.position ="right")
       #p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
       return(p)
@@ -53,17 +53,17 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
               panel.background = element_rect(fill = 'white'),
               panel.grid.major = element_line(colour =   "#D9D9D9",size = size_lines_panel),
               panel.grid.minor = element_line(colour = "#D9D9D9", size = size_lines_panel),
-              panel.border = element_rect(fill=NA,color =  "#414141",size = size_panel_border),
+              panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
               axis.ticks = element_line(colour = "black",size = 2.5),
-              axis.text.x = element_text(family = font_family,face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
-              axis.text.y = element_text(family = font_family,face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
-              axis.title.x = element_text(family = font_family,face="plain",size = size_axis_title,colour = "#414141"),
-              axis.title.y = element_text(family = font_family,face="plain",size=size_axis_title,colour="#414141"),
+              axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text,colour="black",hjust = x_hjust),
+              axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text,colour="black",vjust = y_vjust),
+              axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title,colour = "black"),
+              axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title,colour="black"),
               legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
               legend.title=element_text(size=size_title_text,
-            family = font_family,face = "plain",colour = "#414141"),            legend.key = element_blank(),
+            family = font_family,face = "bold",colour = "black"),            legend.key = element_blank(),
               legend.text = element_text(size=size_legend_text,
-                                         family = font_family,face = "plain",colour = "#414141"),
+                                         family = font_family,face = "bold",colour = "black"),
               legend.position ="right")
       #p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
       return(p)
@@ -73,7 +73,7 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
     p <- ggplot(data = Map_pco,aes_string(x = comp_a,y = comp_b))+
     geom_vline(xintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
     geom_hline(yintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
-      geom_point(size = size,alpha=alpha,pch=mypch,colour="#414141",stroke = stroke,aes(fill = get(col_val)))+
+      geom_point(size = size,alpha=alpha,pch=mypch,colour="black",stroke = stroke,aes(fill = get(col_val)))+
       geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+
       #scale_fill_manual(values = mix.colors)+
       xlab(label = paste(comp_a,"(",myvar[which(names(myvar)==comp_a)],"%)",sep="")) +
@@ -83,17 +83,17 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
             panel.background = element_rect(fill = 'white'),
             panel.grid.major = element_line(colour =   "#D9D9D9",size = size_lines_panel),
             panel.grid.minor = element_line(colour = "#D9D9D9", size = size_lines_panel),
-            panel.border = element_rect(fill=NA,color =  "#414141",size = size_panel_border),
+            panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
             axis.ticks = element_line(colour = "black",size = 2.5),
-            axis.text.x = element_text(family = font_family,face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
-            axis.text.y = element_text(family = font_family,face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
-            axis.title.x = element_text(family = font_family,face="plain",size = size_axis_title,colour = "#414141"),
-            axis.title.y = element_text(family = font_family,face="plain",size=size_axis_title,colour="#414141"),
+            axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text,colour="black",hjust = x_hjust),
+            axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text,colour="black",vjust = y_vjust),
+            axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title,colour = "black"),
+            axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title,colour="black"),
             legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
             legend.title=element_text(size=size_title_text,
-          family = font_family,face = "plain",colour = "#414141"),            legend.key = element_blank(),
+          family = font_family,face = "bold",colour = "black"),            legend.key = element_blank(),
             legend.text = element_text(size=size_legend_text,
-                                       family = font_family,face = "plain",colour = "#414141"),
+                                       family = font_family,face = "bold",colour = "black"),
             legend.position ="right")
     #p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
     return(p)
@@ -110,17 +110,17 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
             panel.background = element_rect(fill = 'white'),
             panel.grid.major = element_line(colour =   "#D9D9D9",size = size_lines_panel),
             panel.grid.minor = element_line(colour = "#D9D9D9", size = size_lines_panel),
-            panel.border = element_rect(fill=NA,color =  "#414141",size = size_panel_border),
+            panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
             axis.ticks = element_line(colour = "black",size = 2.5),
-            axis.text.x = element_text(family = font_family,face = "plain",size =size_axis_text,colour="#414141",hjust = x_hjust),
-            axis.text.y = element_text(family = font_family,face="plain",size=size_axis_text,colour="#414141",vjust = y_vjust),
-            axis.title.x = element_text(family = font_family,face="plain",size = size_axis_title,colour = "#414141"),
-            axis.title.y = element_text(family = font_family,face="plain",size=size_axis_title,colour="#414141"),
+            axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text,colour="black",hjust = x_hjust),
+            axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text,colour="black",vjust = y_vjust),
+            axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title,colour = "black"),
+            axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title,colour="black"),
             legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
             legend.title=element_text(size=size_title_text,
-          family = font_family,face = "plain",colour = "#414141"),            legend.key = element_blank(),
+          family = font_family,face = "bold",colour = "black"),            legend.key = element_blank(),
             legend.text = element_text(size=size_legend_text,
-                                       family = font_family,face = "plain",colour = "#414141"),
+                                       family = font_family,face = "bold",colour = "black"),
             legend.position ="right")
     #p<-p + guides(fill=guide_legend(keywidth=0.5,keyheight=0.5,default.unit="inch",override.aes = list(size=10,stroke=stroke,shape=mypch,alpha=alpha)))
     return(p)
