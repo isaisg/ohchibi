2946180a4eb13bed8dae860ac4db13d31fde4e04
isaisg <isai.salas.gonzalez@gmail.com>
2019-08-13 13:44:23 -0400

    Add skip to oh.read.gff

--- R/oh.read.gff.R
+++ R/oh.read.gff.R
@@ -9,10 +9,10 @@
 
 
 
-oh.read.gff <- function(file = NULL,withOGs = FALSE){
+oh.read.gff <- function(file = NULL,withOGs = FALSE,skip = 1){
   if(withOGs == TRUE){
     tfile <- read.table(file = file,header = F,
-          skip = 1,comment.char = "",quote = "",sep = "\t")
+          skip = skip,comment.char = "",quote = "",sep = "\t")
   colnames(tfile) <-  c("seqid", "source", "type", "start", "end", 
                         "score", "strand", "phase", "attributes","orthogroup_id")
     gid <- NULL
