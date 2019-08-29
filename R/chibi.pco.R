632d5afcefdbe6770ee75dec24fd2b8d6f68fbc0
isaisg <isai.salas.gonzalez@gmail.com>
2019-08-29 18:30:44 -0400

    add mix.colors to chibi.pco

--- R/chibi.pco.R
+++ R/chibi.pco.R
@@ -13,6 +13,7 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
                     ratio_size_shape_background=1.3,y_vjust=0.5,x_hjust=0.5,size_axis_text=20,size_axis_title=30,
                     size_legend_text=20,size_title_text = 30,legend_proportion_size=2,
 		size_lines_panel = 0,size_panel_border = 1,font_family = "Arial"){
+  mix.colors <- c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99","#B15928")
   Map_pco<-list_ohpco$Map_pco
   myvar=list_ohpco$variance_explained
   if(lines_zero == FALSE) {
@@ -20,7 +21,7 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
       p <- ggplot(data = Map_pco,aes_string(x = comp_a,y = comp_b))+
         geom_point(size = size,alpha=alpha,pch=mypch,colour="black",stroke = stroke,aes_string(fill = col_val))+
         geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+
-        #scale_fill_manual(values = mix.colors)+
+        scale_fill_manual(values = mix.colors)+
         xlab(label = paste(comp_a,"(",myvar[which(names(myvar)==comp_a)],"%)",sep="")) +
         ylab(label = paste(comp_b,"(",myvar[which(names(myvar)==comp_b)],"%)",sep="")) +
         guides(fill = guide_legend(override.aes = list(size=12))) +
@@ -78,7 +79,7 @@ chibi.pco<-function(list_ohpco=NULL,col_val=NULL,shape_val=NULL,comp_a="PCo1",co
     geom_hline(yintercept = 0,size = size_axis_line,color = "#D9D9D9",linetype = type_axis_line) +
       geom_point(size = size,alpha=alpha,pch=mypch,colour="black",stroke = stroke,aes_string(fill = col_val))+
       geom_point(size = size/ratio_size_shape_background,colour = col_shape_background,alpha=alpha_shape_background)+
-      #scale_fill_manual(values = mix.colors)+
+      scale_fill_manual(values = mix.colors)+
       xlab(label = paste(comp_a,"(",myvar[which(names(myvar)==comp_a)],"%)",sep="")) +
       ylab(label = paste(comp_b,"(",myvar[which(names(myvar)==comp_b)],"%)",sep="")) +
       guides(fill = guide_legend(override.aes = list(size=12))) +
