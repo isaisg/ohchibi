e05b3eaaa1f084173acdeeaf1702e797c3238741
Isai Salas González <isaisalasgonzalez@Isais-MacBook-Pro.local>
2020-11-14 00:48:40 -0500

    Update theme_ohchibi

--- R/theme_ohchibi.R
+++ R/theme_ohchibi.R
@@ -7,13 +7,13 @@
 #' theme_ohchibi()
 
 
-theme_ohchibi <- function (size_axis_text.x = 20,size_axis_text.y = 20,
-                           size_axis_title.x = 30,size_axis_title.y = 30,
+theme_ohchibi <- function (size_axis_text.x = 15,size_axis_text.y = 15,
+                           size_axis_title.x = 20,size_axis_title.y = 20,
                            angle_text.x = 0,angle_text.y = 0,
                            legend_proportion_size = 1,
-                           size_title_text = 30,size_legend_text = 20,
+                           size_title_text = 15,size_legend_text = 10,
                            size_lines_panel = 0.3,size_panel_border = 1,
-                           x_hjust = 0.5, y_vjust = 0.5,font_family = "Arial",size_ticks = 1.5) { 
+                           x_hjust = 0.5, y_vjust = 0.5,font_family = "Arial",font_face = "plain",size_ticks = 0.5) { 
   theme(
         panel.background = element_rect(fill = 'white'),
         panel.grid.major.y = element_blank(),
@@ -22,16 +22,16 @@ theme_ohchibi <- function (size_axis_text.x = 20,size_axis_text.y = 20,
         panel.border = element_rect(fill=NA,color =  "black",size = size_panel_border),
         axis.line = element_blank(),
 	axis.ticks = element_line(colour = "black",size = size_ticks),
-        axis.text.x = element_text(family = font_family,face = "bold",size =size_axis_text.x,colour="black",hjust = x_hjust,angle = angle_text.x),
-        axis.text.y = element_text(family = font_family,face="bold",size=size_axis_text.y,colour="black",vjust = y_vjust, angle = angle_text.y),
-        axis.title.x = element_text(family = font_family,face="bold",size = size_axis_title.x,colour = "black"),
-        axis.title.y = element_text(family = font_family,face="bold",size=size_axis_title.y,colour="black"),
+        axis.text.x = element_text(family = font_family,face = font_face,size =size_axis_text.x,colour="black",hjust = x_hjust,angle = angle_text.x),
+        axis.text.y = element_text(family = font_family,face=font_face,size=size_axis_text.y,colour="black",vjust = y_vjust, angle = angle_text.y),
+        axis.title.x = element_text(family = font_family,face=font_face,size = size_axis_title.x,colour = "black"),
+        axis.title.y = element_text(family = font_family,face=font_face,size=size_axis_title.y,colour="black"),
         legend.background = element_blank(),legend.key.size = unit(legend_proportion_size,"line"),
         legend.title=element_text(size=size_title_text,
-          family = font_family,face = "bold",colour = "black"),
+          family = font_family,face = font_face,colour = "black"),
         legend.key = element_blank(), 
         legend.text = element_text(size=size_legend_text,
-          family = font_family,face = "bold",colour = "black"),
+          family = font_family,face = font_face,colour = "black"),
         legend.position ="right",
         )
 }
