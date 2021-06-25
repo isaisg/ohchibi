2135a0d2c1c6aa34c0b0c36e3ad3de288ad90d1d
Isai Salas González <isaisalasgonzalez@Isais-MacBook-Pro.local>
2021-06-24 22:39:53 -0400

    Modification top tile

--- R/chibi.heatmap.R
+++ R/chibi.heatmap.R
@@ -5,9 +5,10 @@
 #' @export
 #' @examples
 #' chibi.heatmap()
-chibi.heatmap <- function(Tab = NULL,df_border = NULL,dist_method_rows = "pearson",dist_method_cols = "euclidean",
-                          hclust_method_rows = "complete",hclust_method_cols = "ward.D2",
+chibi.heatmap <- function(Tab = NULL,df_border = NULL,df_tile_col = NULL,dist_method_rows = "pearson",dist_method_cols = "euclidean",
+                          hclust_method_rows = "ward.D",hclust_method_cols = "ward.D",
                           k_rows = 8,k_cols = 4,palette_heatmap = "pals::kovesi.diverging_bwr_55_98_c37",
+                          palette_df_tile_col = paletteer_d("ggthemes::Tableau_20"),
                           range_fill_heatmap = 1,size_strip_text_row = 10,size_strip_text_col = 0,
                           size_axis_text_col = 10,size_axis_text_row = 0,axis_ticks_row = F,
                           size_legend_text = 10, size_legend_title = 12,
@@ -89,10 +90,10 @@ chibi.heatmap <- function(Tab = NULL,df_border = NULL,dist_method_rows = "pearso
       facet_grid(ClusterRows~ClusterCols,scales = "free",space = "free") +
       theme_ohchibi(font_family = font_family
       ) +
-      scale_x_discrete(expand = c(0,0)) + 
+      scale_x_discrete(expand = c(0,0)) +
       scale_y_discrete(expand =c(0,0)) +
       scale_fill_paletteer_c(palette_heatmap,
-                             limits = c(-range_fill_heatmap,range_fill_heatmap),oob = squish,name = "z-score") 
+                             limits = c(-range_fill_heatmap,range_fill_heatmap),oob = squish,name = "z-score")
   }else{
     melted_sub <- merge(melted_sub,df_border, by = c("IdCols","IdRows"),all.x = TRUE)
     colnames(melted_sub)[ncol(melted_sub)] <- "Border"
@@ -103,14 +104,14 @@ chibi.heatmap <- function(Tab = NULL,df_border = NULL,dist_method_rows = "pearso
       facet_grid(ClusterRows~ClusterCols,scales = "free",space = "free") +
       theme_ohchibi(font_family = font_family
       ) +
-      scale_x_discrete(expand = c(0,0)) + 
+      scale_x_discrete(expand = c(0,0)) +
       scale_y_discrete(expand  = c(0,0)) +
       scale_fill_paletteer_c(palette_heatmap,
                              limits = c(-range_fill_heatmap,range_fill_heatmap),oob = squish,name = "z-score") +
       scale_color_manual(values = palette_border,na.value = "#00000000")
   }
   
-
+  
   if(axis_ticks_row == FALSE){
     p_heatmap <-  p +     theme(
       axis.text.y = element_blank(),
@@ -169,25 +170,80 @@ chibi.heatmap <- function(Tab = NULL,df_border = NULL,dist_method_rows = "pearso
   
   p_blank <- ggplot() +theme_void()
   
-  ###### Arrange figures ##########
-  composition <- egg::ggarrange(p_blank,p_tree_cols,
-                                p_tree_rows,p_heatmap,
-                                ncol = 2,nrow = 2,byrow = T,
-                                heights = egg_heights,
-                                widths = egg_widths,padding = unit(20,"line"),draw = F)
-  #Create list to return
-  mres <- list(
-    heatmap = composition,
-    mclust_rows = mclust_rows,
-    mclust_cols = mclust_cols,
-    df_clust_rows = df_clust_rows,
-    df_clust_cols = df_clust_cols,
-    melted = melted_sub,
-    p_dist = p_distri,
-    p_heatmap = p_heatmap,
-    p_tree_rows = p_tree_rows,
-    p_treee_cols = p_tree_cols
-  )
+  
+  if (! is.null(df_tile_col)){
+    #Create structure for col bar
+    df_clust_cols <- merge(df_clust_cols,df_tile_col,by = "IdCols")
+    colnames(df_clust_cols)[ncol(df_clust_cols)] <- "Color"
+    
+    p_col_tile <- ggplot(df_clust_cols,aes(IdCols,"1")) +
+      geom_raster(aes(fill = Color)) +
+      theme_ohchibi() +
+      scale_y_discrete(expand = c(0,0)) +
+      scale_x_discrete(expand = c(0,0))  +
+      facet_grid(.~ClusterCols,space = "free",scales = "free") +
+      theme(
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        strip.text.x  = element_blank(),
+        legend.position = "none",
+        panel.spacing.x  = unit(panel_spacing, "lines"),
+        panel.border = element_rect(size = panel_border_heatmap)
+      ) +
+      scale_fill_manual(values = palette_df_tile_col)
+    
+    #Verify the number of height values passed
+    if (length(egg_heights) < 3){
+      egg_heights <- c(egg_heights[1],0.035,egg_heights[2])
+    }
+
+    ###### Arrange figures ##########
+    composition <- egg::ggarrange(p_blank,p_tree_cols,
+                                  p_blank,p_col_tile,
+                                  p_tree_rows,p_heatmap,
+                                  ncol = 2,nrow = 3,byrow = T,
+                                  heights = egg_heights,
+                                  widths = egg_widths,padding = unit(20,"line"),draw = F)
+    #Create list to return
+    mres <- list(
+      heatmap = composition,
+      mclust_rows = mclust_rows,
+      mclust_cols = mclust_cols,
+      df_clust_rows = df_clust_rows,
+      df_clust_cols = df_clust_cols,
+      melted = melted_sub,
+      p_dist = p_distri,
+      p_heatmap = p_heatmap,
+      p_tree_rows = p_tree_rows,
+      p_treee_cols = p_tree_cols,
+      p_col_tile = p_col_tile
+    )
+    
+  }else{
+    
+    ###### Arrange figures ##########
+    composition <- egg::ggarrange(p_blank,p_tree_cols,
+                                  p_tree_rows,p_heatmap,
+                                  ncol = 2,nrow = 2,byrow = T,
+                                  heights = egg_heights,
+                                  widths = egg_widths,padding = unit(20,"line"),draw = F)
+    #Create list to return
+    mres <- list(
+      heatmap = composition,
+      mclust_rows = mclust_rows,
+      mclust_cols = mclust_cols,
+      df_clust_rows = df_clust_rows,
+      df_clust_cols = df_clust_cols,
+      melted = melted_sub,
+      p_dist = p_distri,
+      p_heatmap = p_heatmap,
+      p_tree_rows = p_tree_rows,
+      p_treee_cols = p_tree_cols
+    )
+  }
+
   return(mres)
 }
-
