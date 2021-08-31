800648f2da57b112b9d6343fc6d54964900c952d
Isai Salas Gonzalez <isaisalasgonzalez@Isais-MacBook-Pro.local>
2021-08-30 20:25:21 -0400

    Change font chibi.anova.R

--- R/chibi.anova.R
+++ R/chibi.anova.R
@@ -8,7 +8,7 @@
 
 chibi.anova <- function(myanova = NULL,pval_thres = 0.05,legend_proportion_size =2,
 y_vjust=1,size_axis_text=20,size_axis_title=30,size_legend_text=20,size_title_text = 30,
-size_ticks_x = 2.5, size_ticks_y =2.5, font_family = "Arial",aspect.ratio =3,size_panel_border = 0.1){
+size_ticks_x = 2.5, size_ticks_y =2.5, font_family = "Helvetica",aspect.ratio =3,size_panel_border = 0.1){
   pval_thres <- pval_thres
   df_aov <- myanova %>% anova %>% as.data.frame
   afss <- df_aov$"Sum Sq"
